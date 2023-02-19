3396461c656888c38ec2cdb19e962c9f6cff0ec1
Sony AK <sony@sony-ak.com>
2023-02-19 14:43:02 +0700

    change Windmill to Airmill

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -347,35 +347,13 @@
 		class:hidden={!isSideMenuOpen}
 		class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"
 	/>
-	<!-- <div
-        x-show="isSideMenuOpen"
-        x-transition:enter="transition ease-in-out duration-150"
-        x-transition:enter-start="opacity-0"
-        x-transition:enter-end="opacity-100"
-        x-transition:leave="transition ease-in-out duration-150"
-        x-transition:leave-start="opacity-100"
-        x-transition:leave-end="opacity-0"
-        class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"
-      ></div> -->
-	<!-- <aside
-        class="fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-white dark:bg-gray-800 md:hidden"
-        class:hidden={isSideMenuOpen}
-        x-transition:enter="transition ease-in-out duration-150"
-        x-transition:enter-start="opacity-0 transform -translate-x-20"
-        x-transition:enter-end="opacity-100"
-        x-transition:leave="transition ease-in-out duration-150"
-        x-transition:leave-start="opacity-100"
-        x-transition:leave-end="opacity-0 transform -translate-x-20"
-        @click.away="closeSideMenu"
-        @keydown.escape="closeSideMenu"
-      > -->
 	<aside
 		class="fixed inset-y-0 z-20 mt-16 w-64 flex-shrink-0 overflow-y-auto bg-white dark:bg-gray-800 md:hidden"
 		class:hidden={!isSideMenuOpen}
 	>
 		<div class="py-4 text-gray-500 dark:text-gray-400">
 			<!-- svelte-ignore a11y-invalid-attribute -->
-			<a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href=""> Windmill </a>
+			<a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="">Airmill</a>
 			<ul class="mt-6">
 				<li class="relative px-6 py-3">
 					<span
@@ -643,8 +621,10 @@
 					</svg>
 				</button>
 				<!-- Search input -->
-				<div class="flex flex-1 justify-center lg:mr-32">
-					<div class="relative mr-6 w-full max-w-xl focus-within:text-purple-500">
+				<!-- <div class="flex flex-1 justify-center lg:mr-32"> -->
+				<div class="flex flex-1 justify-center">
+					<!-- <div class="relative mr-6 w-full max-w-xl focus-within:text-purple-500"> -->
+					<div class="relative mr-6 w-full focus-within:text-purple-500">
 						<div class="absolute inset-y-0 flex items-center pl-2">
 							<svg class="h-4 w-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
 								<path
