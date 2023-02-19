35622f0bc038d7f43e295574c996853bc937a43d
Sony AK <sony@sony-ak.com>
2023-02-20 00:16:59 +0700

    add create account page

--- src/routes/pages/create-account/ContentPagesCreateAccount.svelte
+++ src/routes/pages/create-account/ContentPagesCreateAccount.svelte
@@ -29,14 +29,14 @@
 					<label class="block text-sm">
 						<span class="text-gray-700 dark:text-gray-400">Email</span>
 						<input
-							class="focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input mt-1 block w-full text-sm focus:border-purple-400 focus:outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300"
-							placeholder="Jane Doe"
+							class="form-input mt-1 block w-full rounded-md border-gray-300 text-sm focus:border-purple-400 focus:outline-none focus:ring-[3px] focus:ring-purple-200 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:ring-gray-500"
+							placeholder="email@address.com"
 						/>
 					</label>
 					<label class="mt-4 block text-sm">
 						<span class="text-gray-700 dark:text-gray-400">Password</span>
 						<input
-							class="focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input mt-1 block w-full text-sm focus:border-purple-400 focus:outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300"
+							class="form-input mt-1 block w-full rounded-md border-gray-300 text-sm focus:border-purple-400 focus:outline-none focus:ring-[3px] focus:ring-purple-200 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:ring-gray-500"
 							placeholder="***************"
 							type="password"
 						/>
@@ -44,7 +44,7 @@
 					<label class="mt-4 block text-sm">
 						<span class="text-gray-700 dark:text-gray-400"> Confirm password </span>
 						<input
-							class="focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input mt-1 block w-full text-sm focus:border-purple-400 focus:outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300"
+							class="form-input mt-1 block w-full rounded-md border-gray-300 text-sm focus:border-purple-400 focus:outline-none focus:ring-[3px] focus:ring-purple-200 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:ring-gray-500"
 							placeholder="***************"
 							type="password"
 						/>
@@ -54,37 +54,37 @@
 						<label class="flex items-center dark:text-gray-400">
 							<input
 								type="checkbox"
-								class="focus:shadow-outline-purple dark:focus:shadow-outline-gray form-checkbox text-purple-600 focus:border-purple-400 focus:outline-none"
+								class="form-checkbox text-purple-600 focus:border-purple-400 focus:outline-none focus:ring-[3px] focus:ring-purple-200 dark:focus:ring-gray-500"
 							/>
 							<span class="ml-2">
 								I agree to the
-								<span class="underline">privacy policy</span>
+								<span class="underline cursor-pointer">privacy policy</span>
 							</span>
 						</label>
 					</div>
 
 					<!-- You should use a button here, as the anchor is only used for the example  -->
-					<a
-						class="focus:shadow-outline-purple mt-4 block w-full rounded-lg border border-transparent bg-purple-600 px-4 py-2 text-center text-sm font-medium leading-5 text-white transition-colors duration-150 hover:bg-purple-700 focus:outline-none active:bg-purple-600"
-						href="/pages/login"
+					<button
+						class="mt-4 block w-full rounded-lg border border-transparent bg-purple-600 px-4 py-2 text-center text-sm font-medium leading-5 text-white transition-colors duration-150 hover:bg-purple-700 focus:outline-none focus:ring-[3px] focus:ring-purple-200 active:bg-purple-600"
+						on:click={() => (location.href = '/pages/login')}
 					>
 						Create account
-					</a>
+					</button>
 
 					<hr class="my-8" />
 
 					<button
-						class="focus:shadow-outline-gray flex w-full items-center justify-center rounded-lg border border-gray-300 px-4 py-2 text-sm font-medium leading-5 text-white text-gray-700 transition-colors duration-150 hover:border-gray-500 focus:border-gray-500 focus:outline-none active:bg-transparent active:text-gray-500 dark:text-gray-400"
+						class="flex w-full items-center justify-center rounded-lg border border-gray-300 px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition-colors duration-150 hover:border-gray-500 focus:border-gray-500 focus:outline-none focus:ring-gray-500 active:bg-transparent active:text-gray-500 dark:text-gray-400"
 					>
 						<svg class="mr-2 h-4 w-4" aria-hidden="true" viewBox="0 0 24 24" fill="currentColor">
 							<path
 								d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
 							/>
 						</svg>
-						Github
+						GitHub
 					</button>
 					<button
-						class="focus:shadow-outline-gray mt-4 flex w-full items-center justify-center rounded-lg border border-gray-300 px-4 py-2 text-sm font-medium leading-5 text-white text-gray-700 transition-colors duration-150 hover:border-gray-500 focus:border-gray-500 focus:outline-none active:bg-transparent active:text-gray-500 dark:text-gray-400"
+						class="mt-4 flex w-full items-center justify-center rounded-lg border border-gray-300 px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition-colors duration-150 hover:border-gray-500 focus:border-gray-500 focus:outline-none focus:ring-gray-500 active:bg-transparent active:text-gray-500 dark:text-gray-400"
 					>
 						<svg class="mr-2 h-4 w-4" aria-hidden="true" viewBox="0 0 24 24" fill="currentColor">
 							<path
